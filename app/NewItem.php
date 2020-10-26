8814acf9a4ca831d02abe3dccd2e8bdafb88cb06
MJ119 <manuja115@gmail.com>
2020-10-26 11:57:02 +0100

    -

---
+++ app/NewItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\NewItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|NewItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewItem query()
+ * @mixin \Eloquent
+ */
+class NewItem extends Model
+{
+    //
+}
---
+++ database/migrations/2020_10_25_215515_create_new_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('new_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->boolean('type');
+            $table->text('description');
+           // $table->foreignId(genre_id);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('new_items');
+    }
+}
