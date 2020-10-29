cec348dd96eaa8e4fc87c3f424500121416d1419
MJ119 <manuja115@gmail.com>
2020-10-30 00:02:37 +0100

    added controlllers and files

---
+++ app/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Post
+ *
+ * @property int $id
+ * @property string|null $slug
+ * @property string|null $body
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereSlug($value)
+ * @mixin \Eloquent
+ */
+class Post extends Model
+{
+    //
+}
---
+++ database/migrations/2020_10_26_201406_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->boolean('type');
+            $table->integer('year');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     *///this happens when you rollback the migration, it'll drop the table.
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
